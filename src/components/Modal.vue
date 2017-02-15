b7ae092bbbae3cd4b0f0ecb82056cdf5131dd8ab
Colin Fallon <colinf@mac.com>
2017-02-15 16:46:11 +0000

    Add component code

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,17 +1,31 @@
 <template>
-  <div id='Modal'>
-    
+  <div :class="containerClass">
+    <div :class="{modal: true, in: showModal}" :style="{ display: showModal ? 'block' : 'none' }">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div v-if="this.$slots.header || closeAction" class="modal-header">
+            <button class="close" @click="closeAction">x</button>
+            <slot name="header"></slot>
+          </div>
+          <div v-if="this.$slots.body" class="modal-body">
+            <slot name="body"></slot>
+          </div>
+          <div v-if="this.$slots.footer" class="modal-footer">
+            <slot name="footer"></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div :class="{'modal-backdrop': showModal, in: showModal}"></div>
   </div>
 </template>
 
 <script>
-  import {mapState, mapActions} from 'vuex'
   export default {
-    computed: {
-      ...mapState([])
-    },
-    methods: {
-      ...mapActions([])
+    props: {
+      showModal: Boolean,
+      closeAction: Function,
+      containerClass: String
     }
   }
 </script>
