f77f85f8b894e785115d84f652ccbc5b56560ca9
Miki Maine <mikias.amdu@gmail.com>
2017-08-30 03:40:17 +0300

    Update Component to add support for large modal
    Added a dynamic class with props to check if the large model (modal-lg) is requested.

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="containerClass">
     <div :class="{modal: true, in: showModal}" :style="{ display: showModal ? 'block' : 'none' }">
-      <div class="modal-dialog">
+      <div :class="{'modal-dialog': true, 'modal-lg': largeModal}">
         <div class="modal-content">
           <div v-if="this.$slots.header || closeAction" class="modal-header">
             <button class="close" @click="closeAction">x</button>
@@ -25,7 +25,8 @@
     props: {
       showModal: Boolean,
       closeAction: Function,
-      containerClass: String
+      containerClass: String,
+      largeModal: Boolean
     }
   }
 </script>
